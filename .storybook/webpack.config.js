25b2abe6ffe6fe3850045b8c9d007c7b77087359
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-08-05 08:34:15 +0200

    Adds storybook configuration

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    loaders: [
+      {
+        exclude: [
+          /\.html$/,
+          /\.(js|jsx)(\?.*)?$/,
+          /\.css$/,
+          /\.scss$/,
+          /\.json$/,
+          /\.svg$/
+        ],
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, '../src'),
+        loader: 'babel',
+        query: {
+
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?importLoaders=1!postcss'
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, '../src'),
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file',
+        query: {
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      }
+    ]
+  }
+}
